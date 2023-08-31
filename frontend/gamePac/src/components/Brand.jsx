7b2b79da2cb866fbb9d4a8d94079719898d026f6
Snehal Shyamsukha <snehalshyamsukha@Snehal-Shyamsukha.local>
2023-08-31 08:06:26 +0530

    deleted upper directory

--- frontend/gamePac/src/components/Brand.jsx
+++
@@ -1,9 +0,0 @@
-// import Image from "next/image";
-import Image from '../assets/image.png'
-
-
-const Brand = ({ imgSrc, imgAlt }) => (
-  <Image src={imgSrc} alt={imgAlt} width="258" height="65" />
-);
-
-export default Brand;
