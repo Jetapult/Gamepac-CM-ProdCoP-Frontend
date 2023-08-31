7991085d9cf4fc2c6a92c8fcd24d5be882f8486f
Snehal Shyamsukha <snehalshyamsukha@Snehal-Shyamsukha.local>
2023-08-31 08:01:09 +0530

    new folder structure

---
+++ frontend/gamePac/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import Landing from './components/Landing'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Login from './components/Login'
+import History from './components/History'
+import SummaryPage from "./components/SummaryPage";
+import TranscriptionPage from "./components/TranscriptionPage";
+import ActionPage from "./components/ActionPage";
+import Online from "./components/Online";
+import RecordView from './components/RecordView';
+import Smart from './components/Smart';
+import Assistant from './components/Assistant';
+import Nav from './components/Nav';
+import Record from './components/Record';
+
+function App() {
+  return (
+      <div className='h-screen bg-[#f58174]'>
+         <>
+    <Router>
+    <Navbar />
+    <Routes>
+    <Route exact path="/" element={<Landing />}/>
+    <Route path="/login" element={<Login />}/>
+    <Route path="/home" element={<Home/>}/>
+    <Route path="/history" element={<History/>}/>
+    <Route path="/summary" element={<SummaryPage/>}/>
+    <Route path="/transcription" element={<TranscriptionPage/>}/>
+    <Route path="/actions/:id" element={<ActionPage/>}/>
+    <Route path="/online" element={<Online/>}/>
+    <Route path="/record" element={<RecordView/>}/>
+    <Route path="/smart" element={<Smart/>}/>
+    <Route path="/assistant" element={<Assistant/>}/>
+    <Route path="/recorder" element={<Record />}/>
+    </Routes>
+    </Router>
+    <Router>
+      <Routes>
+        <Route path ="/test" element={<Nav/>}/>
+      </Routes>
+    </Router>
+    </>
+     </div>
+   
+  )
+}
+
+export default App
---
+++ frontend/gamePac/src/components/Online.jsx
@@ -0,0 +1,216 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { auth } from '../config';
+
+const Online = () => {
+  const [file, setFile] = useState(null);
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [userId, setUserId] = useState(null);
+  const [actionId, setActionId] = useState(null);
+  const [selectedPurpose, setSelectedPurpose] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [contributors, setContributors] = useState([]);
+  const [selectedContributors, setSelectedContributors] = useState('');
+  const [token,setToken]=useState('');
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+      setUserId(user.uid);
+      user.getIdToken().then((token)=>{
+        setToken(token);
+      })
+    });
+    return () => unsubscribe();
+  }, []);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/users', {
+          headers: {
+            Authorization: 'Bearer ' + token
+          }
+        });
+        setContributors(response.data);
+      } catch (error) {
+        console.error('Error fetching contributors:', error);
+      }
+    };    
+    fetchUsers();
+  }, [token]);
+  // useEffect(() => {
+  //   // Fetch the list of users from your backend
+  //   axios.get('http://localhost:3000/users')
+  //     .then((response) => {
+  //       setContributors(response.data);
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error fetching contributors:', error);
+  //     });
+  // }, []);
+
+  const id = userId;
+
+  const handlePurposeChange = (event) => {
+    setSelectedPurpose(event.target.value);
+  };
+
+  const handleFileChange = (event) => {
+    const selectedFile = event.target.files[0];
+    setFile(selectedFile);
+  };
+  const handleContributorsChange = (event) => {
+    console.log(event.target.value);
+    setSelectedContributors(event.target.value);
+  };
+  const c=selectedContributors;
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+    if (!file) {
+      alert('Please select a text file.');
+      return;
+    }
+
+    // Read the text file content
+    const reader = new FileReader();
+    reader.onload = async (event) => {
+      const transcription = event.target.result;
+    //   setSummary(transcription);
+
+      if (!selectedPurpose) {
+        alert('Please select a purpose.');
+        return;
+      }
+
+      try {
+        // Step 1: Get the summary
+        const summaryResponse = await axios.post('http://localhost:3000/summary', {
+          transcription,
+        },{
+          headers: {
+            Authorization: 'Bearer ' + token
+          }
+        });
+        const sum = summaryResponse.data.summary;
+        console.log(sum);
+        
+        const response = await axios.post('http://localhost:3000/todos', {
+          transcription,
+        },{
+          headers: {
+            Authorization: 'Bearer ' + token
+          }
+        });
+        const todosList = response.data.todos;
+
+        // Step 2: Save the data to the data table
+        const data = {
+          id,
+          transcription,
+          sum,
+          todosList,
+          p: selectedPurpose,
+          flag:"false",
+          c,
+        };
+
+        const saveData = await axios.post('http://localhost:3000/data', data);
+        const resId = saveData.data.actionId;
+        setActionId(resId);
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error summarizing text:', error);
+        alert('Error summarizing text. Please try again.');
+      }
+    };
+    reader.readAsText(file, 'UTF-8');
+  };
+
+  return (
+    <div className="flex justify-center pt-10">
+      <div className="bg-white p-8  font-['League Spartan'] rounded-md shadow-md w-96 mx-auto ">
+        <h2 className="text-2xl font-bold mb-4">Text to Brief</h2>
+        <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+            <label htmlFor="contributors" className="block font-['League Spartan'] text-sm font-medium text-gray-800">
+              Contributors
+            </label>
+            {contributors.length > 0 && (
+            <select
+              id="contributors"
+              name="contributors"
+              value={selectedContributors}
+              onChange={handleContributorsChange}
+              className="mt-1 px-4 py-2 w-full rounded-md border border-gray-400 focus:outline-none focus:ring focus:border-red-400">
+              <option value="">Select a contributor</option>
+              {contributors.map((contributor) => (
+                <option key={contributor.uid} value={contributor.uid}>
+                  {contributor.email}
+                </option>
+              ))}
+            </select>
+            )}
+          </div>
+          <div className="mb-4">
+            <label htmlFor="purpose" className="block text-sm font-medium text-gray-800">
+              Purpose
+            </label>
+            <select
+              id="purpose"
+              name="purpose"
+              value={selectedPurpose}
+              onChange={handlePurposeChange}
+              className="mt-1 px-4 py-2 w-full rounded-md border border-gray-400 focus:outline-none focus:ring focus:border-red-400"
+            >
+              <option value="">Select a purpose</option>
+              <option value="Investment">Investment</option>
+              <option value="Hiring">Hiring</option>
+              <option value="Game Analysis">Game Analysis</option>
+              <option value="Share Holders Connect">Share Holders Connect</option>
+            </select>
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="textFile" className="block text-sm font-medium text-gray-800">
+              Upload Text File
+            </label>
+            <input
+              type="file"
+              id="textFile"
+              name="textFile"
+              accept=".txt"
+              onChange={handleFileChange}
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-[#f58174] text-white py-2 px-4 rounded-md hover:bg-[#eaa399] focus:outline-none focus:ring focus:border-gray-400"
+          >
+            Give Action Items
+          </button>
+        </form>
+      </div>
+      <div>
+      {isLoading? (
+  <button className="w-full bg-[#f1efe7]  py-2 px-4 rounded-md cursor-not-allowed opacity-50" disabled>Loading...</button>
+) :null}
+        {actionId && (
+          <button
+            className="w-full bg-[#f1efe7]  py-2 px-4 rounded-md hover:bg-[#eaa399] focus:outline-none focus:ring focus:bg-[#eaa399]"
+            onClick={() => navigate(`/actions/${actionId}`)}
+          >
+            View Action Items
+          </button>
+        )}
+      </div>
+
+    </div>
+  );
+};
+
+export default Online;
