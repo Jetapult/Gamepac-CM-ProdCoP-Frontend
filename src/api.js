995fe5c398740bdfc4463efb65659f9007ad7105
Snehal Shyamsukha <snehalshyamsukha@Snehal-Shyamsukha.local>
2023-09-25 12:55:41 +0530

    edited typo

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:3000',
- baseUrl: 'https://api.gamepacai.com/',
+ baseURL: 'https://api.gamepacai.com/',
 });
 
 export default api;
