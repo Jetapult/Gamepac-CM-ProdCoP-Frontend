37476d9546445d2087e115b06168bc2abb6cdffb
Swathi Shekar <swatipoojary@Swati-Poojary.local>
2024-06-04 16:04:15 +0530

    backend link changes

--- src/api.js
+++ src/api.js
@@ -4,8 +4,8 @@ import { getAuthToken } from "./utils";
 const token = getAuthToken()?.token;
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
-  // baseURL: "https://api.gamepacai.com/",
+  // baseURL: 'http://localhost:3000',
+  baseURL: "https://api.gamepacai.com/",
   timeout: 600000, // Increase the timeout to 10 minutes
   headers: {
     ...token ? { 'Authorization': `Bearer ${token}` } : {}
