83e297e5c4f45d2dabd8af25b5379e9aeb1364d4
Snehal Shyamsukha <snehal.shyamsukha@jetapult.me>
2024-10-22 17:57:35 +0530

    update: revamped landing

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,14 @@ export default {
       },
       fontFamily: {
         sans: ['SF Pro Display', ...defaultTheme.fontFamily.sans],
+        'sf-pro-display': ['SF Pro Display', 'sans-serif'],
      },
+     fontWeight: {
+      'sf-regular': 400,
+      'sf-semibold': 600,
+      'sf-bold': 700,
+      'sf-black': 900,
+    },
     },
   },
   plugins: [],
