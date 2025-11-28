d2a8711b2a52de751de6d38f0beabaf3bc071d23
Rushikesh Suryawanshi <rushikesh@fastcode.ai>
2025-11-28 19:00:44 +0530

    Added building animations

--- src/SuperAgent/components/PreviewPanel.jsx
+++ src/SuperAgent/components/PreviewPanel.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import TaskProgress from "./TaskProgress";
+import ZeroStateAnimation from "./ZeroStateAnimation";
 
 const PreviewPanel = ({
   currentTask,
@@ -12,14 +13,9 @@ const PreviewPanel = ({
       {/* Zero State / Building State */}
       <div className="flex-1 flex items-center justify-center">
         <div className="text-center">
-          {/* Illustration */}
-          <div className="w-[296px] h-[177px] bg-white rounded-lg mb-6 mx-auto relative overflow-hidden">
-            <div className="absolute top-[43px] left-3 w-[100px] h-3 bg-[#f8f8f7] rounded-sm" />
-            <div className="absolute top-[69px] left-3 w-[100px] h-3 bg-[#f8f8f7] rounded-sm" />
-            <div className="absolute top-[89px] left-3 w-[100px] h-20 bg-[#f8f8f7] rounded-sm" />
-            <div className="absolute top-[43px] left-[124px] w-[163px] h-20 bg-[#f8f8f7] rounded-sm" />
-            <div className="absolute top-[129px] left-[124px] w-[160px] h-3 bg-[#f8f8f7] rounded-sm" />
-            <div className="absolute top-[147px] left-[124px] w-[160px] h-[22px] bg-[#f8f8f7] rounded-sm" />
+          {/* Animated Illustration */}
+          <div className="mb-6">
+            <ZeroStateAnimation />
           </div>
 
           <p
